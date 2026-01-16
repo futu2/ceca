2826048a3d5746ca3c19e92371f658166f12895b
futu <you@example.com>
2026-01-16 10:53:51 +0800

    add lsp basis

--- lib/Ceca/LSP.hs
+++ lib/Ceca/LSP.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Ceca.LSP where
+
+import Control.Monad.IO.Class
+import Data.Text qualified as T
+import Language.LSP.Protocol.Message
+import Language.LSP.Protocol.Types
+import Language.LSP.Server
 
 -- Placeholder LSP server for record field autocompletion
 runLSPServer :: IO ()
